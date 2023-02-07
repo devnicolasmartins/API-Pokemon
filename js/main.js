722004b1e203d470aef56fffcdc0d4ae4e0b10c2
Nícolas dos Santos Martins <47349@tjce.jus.br>
2023-02-07 18:51:53 -0300

    Tratamente de erros e início da função que irá adicionar tipos nos pokemons

--- js/main.js
+++ js/main.js
@@ -3,28 +3,28 @@ async function getPokemonInfo(){
     var selection = document.getElementById("pokedex");
     var pokeinfo = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1008&offset=0");
     var pokemon  = pokeinfo.data.results;
-    pokemon.forEach(pokemon =>{
+    pokemon.forEach(async pokemon =>{
       createDiv(pokemon);
-      console.log(pokemon);
-      selection.appendChild(createDiv(pokemon))
+      selection.appendChild(createDiv(pokemon));
     });
   } catch(error){
     console.error(`Erro ao buscar informações sobre o Pokémon: ${error}`);
   }
 }
 function createDiv(pokemon){
-  div = document.createElement("div");
-  div.setAttribute("class", "info-pokemon text-center col-3")
-  p = document.createElement("p");
-  p.setAttribute("class", "name-of-pokemon");
+  principalDiv = document.createElement("div");
+  principalDiv.setAttribute("class", "info-pokemon text-center col-3")
+  infoDiv = document.createElement("div");
+  infoDiv.setAttribute("class", "name-of-pokemon");
   var pokemonName= pokemon.name.toLowerCase().split(" ").map(name => name.charAt(0).toUpperCase() + name.slice(1)).join(" ");
-  p.textContent=pokemonName;
-  div.appendChild(p);
-  div.appendChild(createImg(pokemon));
+  infoDiv.textContent=pokemonName;
+  principalDiv.appendChild(infoDiv);
+  principalDiv.appendChild(createImg(pokemon));
 
-  return div;
+  return principalDiv;
 }
 function createImg(pokemon){
+  try{
   var img = document.createElement("img");
   var pokemonId = pokemon.url.split("/")[6];
   if (pokemonId.length === 1) {
@@ -33,10 +33,30 @@ function createImg(pokemon){
   else if (pokemonId.length === 2){
     pokemonId = "0" + pokemonId;
   }
-  img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonId}.png`;
-  // img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`;
+  img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonId}.png`; 
+  //img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`;
   img.setAttribute("class", "pokeImg");
+  } catch(error){
+    console.error(`Erro ao buscar imagens do Pokémon: ${error}`);
+  }
   return img;
 }
-
+function createType(pokemon){
+  var types = document.createElement("div");
+  var id = pokemon.url.split("/")[6];
+  try{
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(function(pokeType){
+      var pokemonTypes = pokeType.data.types;
+      var typesArray = [];
+      pokemonTypes.forEach( pokemonTypes =>{
+        typesArray.push(pokemonTypes.type.name)
+      })
+      types.textContent = typesArray.join(", ");
+      resolve(types);
+    });
+  }catch(error){
+    console.log(`Erro ao adicionar os tipos dos pokemons: ${error}`)
+  }
+}
 getPokemonInfo();
