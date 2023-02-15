17fd9e4b26d81411edd534aacd8ea17c7dd6ad0c
Nícolas dos Santos Martins <47349@tjce.jus.br>
2023-02-15 18:12:04 -0300

    Início da montagem dos modais que abrem ao clicar em um pokemons, mostrando suas informações

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var pagination = 0;
+
 document.getElementById("nextBtn").addEventListener("click", function(){
   if (pagination >= 1008){
     return;
@@ -21,6 +22,9 @@ async function getPokemonInfo(offset){
 function createDiv(pokemon){
   var principalDiv = document.createElement("div");
   principalDiv.setAttribute("class", "info-pokemon text-center col-3")
+  principalDiv.addEventListener("click", function(){
+    openModal(pokemon);
+  });
 
   var infoDiv = document.createElement("div");
   infoDiv.setAttribute("class", "name-of-pokemon");
@@ -48,7 +52,7 @@ function createImg(pokemon){
   else if (pokemonId.length === 2){
     pokemonId = "0" + pokemonId;
   }
-  img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonId}.png`; 
+  img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemonId}.png`; 
   //img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`;
   img.setAttribute("class", "pokeImg");
   } catch(error){
@@ -103,6 +107,86 @@ function createId(pokemon){
   return id;
 }
 
+function createModall(pokemon){
+  var modalId = `modal-${pokemon.name}`;
+  var modal = document.createElement("div");
+  modal.setAttribute("class", "modal fade");
+  modal.setAttribute("id", modalId);
+  modal.setAttribute("tabindex", "-1");
+  modal.setAttribute("aria-labelledby", "${modalId}-label");
+  modal.setAttribute("aria-hidden", "true");
+
+  var dialog = document.createElement("div");
+  dialog.setAttribute("class", "modal-dialog modal-lg modal-dialog-centered");
+
+  var content = document.createElement("div");
+  content.setAttribute("class", "modal-content");
+
+  var body = document.createElement("div");
+  body.setAttribute("class", "modal-body");
+
+  var title = document.createElement("h4");
+  title.setAttribute("class", "title-modal title-modal-adjust")
+  title.setAttribute("id", `${modalId}-label`)
+  title.textContent = pokemon.name.split(" ").map(name => name.charAt(0).toUpperCase() + name.slice(1)).join(" ");
+
+  var showId= createId(pokemon);
+  showId.classList.add("id-modal");
+  showId.classList.remove("pokemon-id")
+
+  var textContainer= document.createElement("div");
+  textContainer.setAttribute("class", "text-container")
+  textContainer.appendChild(title);
+  textContainer.appendChild(showId);
+  body.appendChild(textContainer)
+
+  var closeBtn = document.createElement("button");
+  closeBtn.setAttribute("type", "button");
+  closeBtn.setAttribute("class", "btn-close my-close-btn");
+  closeBtn.setAttribute("data-bs-dismiss", "modal");
+  closeBtn.setAttribute("aria-label", "Close");
+
+  var imgContainer = document.createElement("div");
+  imgContainer.setAttribute("class", "img-container");
+
+  var pokemonImg = createImg(pokemon);
+  imgContainer.appendChild(pokemonImg);
+  pokemonImg.classList.add("modal-img")
+  pokemonImg.classList.remove("pokeImg")
+  
+  var typeList = document.createElement("div");
+  typeList.classList.add("type-list")
+
+  createType(pokemon).then(types =>{
+    typeBg = types[0].textContent;
+    imgContainer.classList.add(`type-${typeBg}-bg`)
+    types.forEach(typeDiv=>{
+      typeList.appendChild(typeDiv);
+      body.appendChild(typeList);
+      body.appendChild(imgContainer);
+      typeDiv.classList.add("types-of-pokemon-modal")
+    });
+  });
+
+  
+  body.appendChild(closeBtn);
+  content.appendChild(body);
+  content.appendChild(body);
+  dialog.appendChild(content);
+  modal.appendChild(dialog);
+
+  return modal;
+}
+
+function openModal(pokemon){
+  var modalId = `modal-${pokemon.name}`;
+  var selection = document.getElementById("pokedex");
+  var modal = createModall(pokemon);
+  selection.appendChild(modal);
+  var modalElement = new bootstrap.Modal(document.getElementById(modalId));
+  modalElement.show();
+}
+
 var inputPokemon = document.getElementById("floatingInput");
 inputPokemon.addEventListener("input", searchPokemon);
 var allPokemon = [];
@@ -133,5 +217,6 @@ async function searchPokemon(){
     selection.appendChild(await createDiv(pokemon));
   });
 }
+
 getAllPokemon(limit);
 getPokemonInfo();
